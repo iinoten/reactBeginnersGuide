5cd9c013ef1a5cf48c9663c190cad8718f8ab195
Iino Ten <nteten0313@icloud.com>
2018-11-15 19:29:42 +0900

    2章やった

---
+++ from-qiita/src/components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react';  //Reactを使うときのおまじない
+
+const Title = () => {
+    return(
+        <div>
+            <h2>Hello World2.0 </h2>
+        </div>
+    )
+}
+
+export default Title;
